2efcd86f7a7d9da5f843d51b770f37e64566ec56
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2020-04-14 20:41:49 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+import pkg_resources
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup_requires = []
+install_requires = [
+    'opencv-python>=4.1.2.30',
+    'numpy>=1.16',
+    'matplotlib>=2.1.2',
+    'keras',
+    'tensorflow',
+]
+
+
+setup(
+    name='image_to_image',
+    version='0.0.0',
+    description='',
+    author='Krishneel Chaudhary',
+    author_email='krishneel@krishneel',
+    license='None',
+    packages=find_packages(),
+    setup_requires=setup_requires,
+    install_requires=install_requires
+)
